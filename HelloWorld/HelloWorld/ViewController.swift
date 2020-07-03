effbbdd55465cc6f229bde228e63bf8b7bf1c68b
N.C Wu <mythnc@gmail.com>
2020-07-03 23:07:48 +0900

    Ch3 - hw2
    - Customize display message

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -16,9 +16,34 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        present(alertController, animated: true, completion: nil)
+        // 初始化一個存放表情符號的字典
+        // 如果你忘記怎麼做，請參考一下前面的章節
+        // 程式碼填入至下方
+        let buttonMap = [
+            "👾": "alien",
+            "👻": "ghost",
+            "🤖": "robot",
+            "🤓": "coolman",
+        ]
+
+        // sender 是使用者所按下的按鈕
+        // 這裡我們將 sender 儲存至 selectedButton constant
+        let selectedButton = sender
+
+        // 從所選按鈕的標題標籤取得表情符號
+        if let wordToLookup = selectedButton.titleLabel?.text {
+
+            // 從字典取得表情符號的意義
+            // 程式碼填入至下方
+            let meaning = buttonMap[wordToLookup]
+
+            // 變更以下這行程式將 Hello World 的顯示以來表情符號的意義來取代
+            let alertController = UIAlertController(title: "Meaning", message: meaning, preferredStyle: UIAlertController.Style.alert)
+
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+
     }
 }
 
